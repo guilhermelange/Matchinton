be293e5172f45808117928d5cce539631667e292
Guilherme Luiz Lange <gui.luizlange@gmail.com>
2023-05-17 20:39:40 -0300

    public decorator

--- src/models/user/user.controller.ts
+++ src/models/user/user.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Public } from 'src/common/decorator/publicDecorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @Public()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
