863097ee764394ee3c8875e3d329365bf84dbb8a
Guilherme Luiz Lange <gui.luizlange@gmail.com>
2023-06-20 20:59:17 -0300

    new: Added users unit test

--- src/models/user/user.controller.spec.ts
+++ src/models/user/user.controller.spec.ts
@@ -1,10 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { PrismaModule } from '../../database/prisma';
+import { PrismaModule, User } from '../../database/prisma';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+const newUser: User = {
+  id: BigInt(1),
+  email: '',
+  name: '',
+  password: '',
+  phone: '',
+  created_at: new Date(),
+  type: 'USER',
+  updated_at: new Date(),
+  username: '',
+};
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,9 +29,99 @@ describe('UserController', () => {
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create user and return without password', async () => {
+      // Arrange
+      const body: CreateUserDto = {
+        email: '',
+        name: '',
+        password: '',
+        phone: '',
+        username: '',
+      };
+      jest.spyOn(service, 'create').mockResolvedValue(newUser);
+
+      // Act
+      const result = await controller.create(body);
+
+      // Assert
+      expect(result).toEqual(newUser);
+      expect(service.create).toHaveBeenCalledTimes(1);
+      expect(service.create).toHaveBeenCalledWith(body);
+      expect(result.password).toBeFalsy();
+    });
+
+    it('should throw an exception for bad request', () => {
+      // Arrange
+      const body = {
+        email: '',
+        name: '',
+      };
+      jest.spyOn(service, 'create').mockRejectedValueOnce(new Error());
+
+      // Assert
+      expect(controller.create(body as CreateUserDto)).rejects.toThrowError();
+    });
+  });
+
+  describe('update', () => {
+    it('should update a user successfully', async () => {
+      // Arrange
+      const body: UpdateUserDto = {
+        email: 'gui.luizlange@gmail.com',
+      };
+      jest.spyOn(service, 'update').mockResolvedValue('');
+
+      // Act
+      const result = await controller.update('1', body);
+
+      // Assert
+      expect(result).toEqual('');
+      expect(service.update).toHaveBeenCalledTimes(1);
+      expect(service.update).toHaveBeenCalledWith(1, body);
+    });
+
+    it('should throw an exception', () => {
+      // Arrange
+      const body: UpdateUserDto = {
+        type: 'ADMIN',
+      };
+
+      jest.spyOn(service, 'update').mockRejectedValueOnce(new Error());
+
+      // Assert
+      expect(controller.update('1', body)).rejects.toThrowError();
+    });
+  });
+
+  describe('delete', () => {
+    it('should remove a user successfully', async () => {
+      // Arrange
+      jest.spyOn(service, 'remove').mockResolvedValue('');
+
+      // Act
+      const result = await controller.remove('1');
+
+      // Assert
+      expect(result).toBe('');
+      expect(service.remove).toBeCalledTimes(1);
+      expect(service.remove).toBeCalledWith(1);
+    });
+
+    it('should throw an exception', () => {
+      // Arrange
+      jest.spyOn(service, 'remove').mockRejectedValueOnce(new Error());
+
+      // Assert
+      expect(controller.remove('1')).rejects.toThrowError();
+    });
   });
 });
--- src/models/user/user.service.spec.ts
+++ src/models/user/user.service.spec.ts
@@ -1,9 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { HttpException } from '@nestjs/common';
 import { UserService } from './user.service';
-import { PrismaModule } from '../../database/prisma';
+import { PrismaModule, PrismaService, User } from '../../database/prisma';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+const newUser: User = {
+  id: BigInt(1),
+  email: '',
+  name: '',
+  password: '',
+  phone: '',
+  created_at: new Date(),
+  type: 'USER',
+  updated_at: new Date(),
+  username: 'guilhermelange',
+};
 
 describe('UserService', () => {
   let service: UserService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +28,106 @@ describe('UserService', () => {
     }).compile();
 
     service = module.get<UserService>(UserService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a new todo entity item successfully', async () => {
+      // Arrange
+      const data: CreateUserDto = {
+        email: '',
+        name: '',
+        password: '',
+        phone: '',
+        username: 'guilhermelange',
+      };
+
+      prisma.user.findFirst = jest.fn().mockResolvedValueOnce(undefined);
+      prisma.user.create = jest.fn().mockResolvedValueOnce(newUser);
+
+      // Act
+      const result = await service.create(data);
+
+      // Assert
+      expect(result).toEqual(newUser);
+      expect(result.password).toBeFalsy();
+      expect(prisma.user.create).toHaveBeenCalledTimes(1);
+      expect(typeof result).toEqual('object');
+    });
+
+    it('should throw an exception', () => {
+      // Arrange
+      const data: CreateUserDto = {
+        email: '',
+        name: '',
+        password: '',
+        phone: '',
+        username: 'guilhermelange',
+      };
+
+      prisma.user.findFirst = jest.fn().mockResolvedValueOnce(newUser);
+
+      // Assert
+      expect(service.create(data)).rejects.toThrowError();
+    });
+  });
+
+  describe('update', () => {
+    it('should update a user successfully', async () => {
+      // Arrange
+      const data: UpdateUserDto = {
+        email: 'gui.luizlange@gmail.com',
+      };
+
+      prisma.user.findUnique = jest.fn().mockResolvedValueOnce(newUser);
+      prisma.user.update = jest.fn().mockResolvedValueOnce('');
+
+      // Act
+      const result = await service.update(1, data);
+
+      // Assert
+      expect(result).toEqual('');
+      expect(prisma.user.update).toBeCalledTimes(1);
+    });
+
+    it('should throw a not found exception', () => {
+      // Arrange
+      prisma.user.findUnique = jest.fn().mockResolvedValueOnce(undefined);
+
+      const data: UpdateUserDto = {
+        email: 'gui.luizlange@gmail.com',
+      };
+
+      // Assert
+      expect(service.update(1, data)).rejects.toThrowError();
+    });
+  });
+
+  describe('remove', () => {
+    it('should delete a user successfully', async () => {
+      // Arrange
+      prisma.user.findUnique = jest.fn().mockResolvedValueOnce(newUser);
+      prisma.user.delete = jest.fn().mockResolvedValue('');
+
+      // Act
+      const result = await service.remove(1);
+
+      // Assert
+      expect(result).toBeFalsy();
+      expect(prisma.user.delete).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw a not found exception', async () => {
+      // Arrange
+      prisma.user.findUnique = jest.fn().mockResolvedValueOnce(undefined);
+
+      // Assert
+      expect(service.remove(1)).rejects.toThrowError();
+      expect(prisma.user.findUnique).toBeCalledTimes(1);
+    });
   });
 });
